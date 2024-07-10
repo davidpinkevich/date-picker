eae8591f1b1b161d64d1c882270ace34059e01c1
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-10 16:05:11 +0300

    feat: update config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,34 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
-const path = require("path");
+import path from "node:path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  webpackFinal: async (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config?.resolve?.alias,
+        assets: path.resolve(__dirname, "../src/assets"),
+        components: path.resolve(__dirname, "../src/components"),
+        constants: path.resolve(__dirname, "../src/constants"),
+        hooks: path.resolve(__dirname, "../src/hooks"),
+        utils: path.resolve(__dirname, "../src/utils"),
+        styles: path.resolve(__dirname, "../src/styles")
+      }
+    };
+
+    return config;
+  },
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/preset-scss"
+    "@storybook/addon-interactions"
+    // "@storybook/preset-scss"
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,11 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
 import url from "@rollup/plugin-url";
-import alias from "@rollup/plugin-alias";
-
-const path = ["components", "utils"];
 
 export default {
   input: "src/index.ts",
@@ -27,17 +23,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      modules: true,
-      use: ["sass"],
-      minimize: true
-    }),
-    url(),
-    alias({
-      entries: path.map((item) => ({
-        find: `@${item}`,
-        replacement: `./src/${item}`
-      }))
-    })
-  ]
+    url()
+  ],
+  external: ["react", "react-dom", "styled-components"]
 };
