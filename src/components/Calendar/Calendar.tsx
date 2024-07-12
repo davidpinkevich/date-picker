a0b2f6762a153d2416128d2fb9de282c4b9c6463
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-12 14:43:01 +0300

    feat: add calendar

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { useCalendar } from "hooks/useCalendar";
+
+import { DateBlock } from "components/DateBlock/DateBlock";
+import { Input } from "components/Input/Input";
+import { ThemeContainer } from "components/ThemeContainer/ThemeContainer";
+
+import { StyledCalendar } from "./styled";
+
+const Calendar: React.FC<{ typeStart: "Mo" | "Su" }> = ({ typeStart }) => {
+  const {
+    value,
+    setValue,
+    dateContainer,
+    typeSwitch,
+    targetDate,
+    setTargetDate
+  } = useCalendar({ typeStart });
+
+  return (
+    <ThemeContainer>
+      <StyledCalendar>
+        <h2>Calendar</h2>
+        <Input value={value} setValue={setValue} />
+        <DateBlock
+          data={dateContainer}
+          typeSwitch={typeSwitch}
+          typeStart={typeStart}
+          targetDate={targetDate}
+          setTargetDate={setTargetDate}
+        />
+      </StyledCalendar>
+    </ThemeContainer>
+  );
+};
+
+export { Calendar };
