62a1bcf50f663cb5ca98b19984c7ec6a3c80a5fa
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-12 14:41:16 +0300

    feat: update constants for weeks

--- src/constants/daysOfWeek.ts
+++
@@ -1,3 +0,0 @@
-const DAYS_OF_WEEK = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
-
-export { DAYS_OF_WEEK };
--- src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-import { DAYS_OF_WEEK } from "./daysOfWeek";
-
-export { DAYS_OF_WEEK };
