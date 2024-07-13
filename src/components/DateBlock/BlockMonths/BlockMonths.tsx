440f0e374f9859a1ea1d17c43bec59be88b652c2
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-13 21:35:48 +0300

    feat: add block months

---
+++ src/components/DateBlock/BlockMonths/BlockMonths.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+
+import { Context } from "constants/context";
+import { MONTHS } from "constants/data";
+
+import { StyledBlockWeeks, StyledBlockWeeksItem } from "./BlockMonths.styled";
+
+const BlockMonths: React.FC = ({}) => {
+  const { targetDate, setTargetDate, setTypeSwitch } = useContext(Context);
+
+  const handleSwitch = (month: number) => {
+    setTargetDate({ ...targetDate, month });
+    setTypeSwitch("weeks");
+  };
+
+  return (
+    <StyledBlockWeeks>
+      {MONTHS.map((item) => {
+        return (
+          <StyledBlockWeeksItem
+            onClick={() => handleSwitch(item.id)}
+            target={targetDate.month === item.id}
+            key={item.id}>
+            {item.name}
+          </StyledBlockWeeksItem>
+        );
+      })}
+    </StyledBlockWeeks>
+  );
+};
+
+export { BlockMonths };
