461c1803b510ca6e2e4821d58fd5477c2dc0c988
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-15 12:11:18 +0300

    feat: app constants for switch

--- src/constants/data/index.ts
+++ src/constants/data/index.ts
@@ -1,6 +1,7 @@
+import { BASE_PERIOD } from "./basePeriod";
 import { DAYS_OF_WEEK } from "./daysOfWeek";
 import { MONTHS } from "./monthsOfWeek";
-import { BASE_PERIOD } from "./basePeriod";
 import { StartDays } from "./startDays";
+import { TypesSwitch } from "./typesSwitch";
 
-export { StartDays, DAYS_OF_WEEK, MONTHS, BASE_PERIOD };
+export { BASE_PERIOD, DAYS_OF_WEEK, MONTHS, StartDays, TypesSwitch };
