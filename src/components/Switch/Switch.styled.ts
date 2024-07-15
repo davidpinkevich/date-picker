ccac1adbec3f0e761acd4f3ac5d7635703b48fc3
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-15 12:06:39 +0300

    feat: add styles switch

---
+++ src/components/Switch/Switch.styled.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+const StyledSwitch = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+  justify-content: space-between;
+`;
+
+const StyledSwitchBlock = styled.div`
+  cursor: pointer;
+  transition: all 0.2s;
+  border-radius: 8px;
+  font-weight: 700;
+  padding: 5px 10px;
+  &:hover {
+    transform: scale(1.03);
+    background-color: ${(props) => props.theme.colors.lightGray};
+  }
+`;
+
+const StyledSwitchTitle = styled.p`
+  padding: 10px 15px;
+`;
+
+const StyledArrowLeft = styled.button`
+  display: flex;
+  background-color: inherit;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+  border-radius: 8px;
+  padding: 5px;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.lightGray};
+  }
+`;
+
+const StyledArrowRight = styled.button`
+  display: flex;
+  background-color: inherit;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+  border-radius: 8px;
+  padding: 5px;
+  &:hover {
+    background-color: ${(props) => props.theme.colors.lightGray};
+  }
+
+  & svg {
+    transform: rotate(180deg);
+  }
+`;
+
+export {
+  StyledSwitch,
+  StyledSwitchBlock,
+  StyledSwitchTitle,
+  StyledArrowLeft,
+  StyledArrowRight
+};
