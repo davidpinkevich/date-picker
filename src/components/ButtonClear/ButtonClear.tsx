dde7eb663c3bd323f400cb3805fdb5c7991a6380
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-16 16:45:45 +0300

    feat: add btn clear

---
+++ src/components/ButtonClear/ButtonClear.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { StyledButtonClear } from "./ButtonClear.styled";
+
+import { type PropsButtonClear } from "./ButtonClear.types";
+
+const ButtonClear: React.FC<PropsButtonClear> = ({ onClick, label }) => {
+  return <StyledButtonClear onClick={onClick}>{label}</StyledButtonClear>;
+};
+
+export { ButtonClear };
