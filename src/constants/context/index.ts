e98e5ceb54dafddc1ffc1d6b112c83780ef17a45
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-17 19:18:50 +0300

    refactor: clear context

--- src/constants/context/index.ts
+++ src/constants/context/index.ts
@@ -4,20 +4,19 @@ import { type TypeContext } from "types/contextTypes";
 
 const Context = createContext<TypeContext>({
   typeStart: "Mo",
-  targetDay: null,
   withHolidays: false,
   mainPeriod: { min: 2020, max: 2026 },
   setDateContainer: () => {},
   setWithHolidays: () => {},
   setMainPeriod: () => {},
-  setTargetDay: () => {},
   setTypeStart: () => {},
   typeSwitch: "weeks",
   setTypeSwitch: () => {},
   targetDate: {
     years: 2020,
     year: 2024,
-    month: 6
+    month: 6,
+    day: null
   },
   setTargetDate: () => {},
   dateContainer: []
