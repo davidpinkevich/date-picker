23a3bd70104dbe92ab1875f2e733ada5f49a3308
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-19 12:35:49 +0300

    fear: add styles todo list

---
+++ src/components/TodoList/TodoList.styled.ts
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+const StyledTodoList = styled.div<{ $viewTodo: boolean }>`
+  position: absolute;
+  background-color: ${(props) => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.mainDark};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid #bbbbbb;
+  padding: 15px;
+  border-radius: 8px;
+  width: 100%;
+  height: 100%;
+  right: ${(props) => (props.$viewTodo ? "0%" : "-100%")};
+  top: 0;
+  transition: all 0.2s;
+`;
+
+const StyledTodoUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 0px;
+  }
+
+  &::-ms-scrollbar {
+    width: 0px;
+  }
+`;
+
+const StyledCloseBtn = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  background-color: inherit;
+  width: 20px;
+  height: 20px;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.15);
+  }
+
+  & svg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const StyledAddBtn = styled.button`
+  background-color: inherit;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.colors.lightGray};
+  font-size: ${(props) => props.theme.fontSizes.todoListBtnAdd};
+  font-weight: 600;
+  width: 100%;
+  padding: 10px 15px;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const StyledTodoInput = styled.input`
+  font-size: ${(props) => props.theme.fontSizes.todoListInput};
+  border: 1px solid #bbbbbb;
+  padding: 10px 15px;
+  border-radius: 8px;
+  width: 100%;
+  transition: all 0.2s;
+
+  &:focus {
+    box-shadow: inset 0px 0px 1px 1px #bbbbbb;
+  }
+`;
+
+export {
+  StyledAddBtn,
+  StyledCloseBtn,
+  StyledTodoInput,
+  StyledTodoList,
+  StyledTodoUl
+};
--- src/decorators/withDatePicker/styled.ts
+++ src/decorators/withDatePicker/styled.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 const StyledWrapperDatePicker = styled.div`
+  position: relative;
   width: 250px;
+  overflow: hidden;
 `;
 
 export { StyledWrapperDatePicker };
