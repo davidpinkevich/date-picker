c3b9b6cc813e9966fdb41196e65f8312c144d363
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-19 12:37:12 +0300

    feat: add styles todo list header

---
+++ src/components/TodoList/TodoHeader/TodoHeader.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const StyledTodoHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const StyledTodoHeaderTitile = styled.h2`
+  font-size: ${(props) => props.theme.fontSizes.todoListTitle};
+  font-weight: 600;
+  text-align: center;
+`;
+
+const StyledTodoHeaderText = styled.p`
+  font-weight: 600;
+`;
+
+export { StyledTodoHeader, StyledTodoHeaderTitile, StyledTodoHeaderText };
