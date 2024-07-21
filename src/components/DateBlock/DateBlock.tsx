b02e45ac17bb35ab61b832f2a98c17b9cb7fe541
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-21 16:23:33 +0300

    refactor: remove memo

--- src/components/DateBlock/DateBlock.tsx
+++ src/components/DateBlock/DateBlock.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 
 import { Context } from "constants/context";
 import { DAYS_OF_WEEK, StartDays, TypesSwitch } from "constants/data";
@@ -15,70 +15,67 @@ import {
 } from "./DateBlock.styled";
 import { type PropsDateBlock } from "./DateBlock.types";
 
-const DateBlock: React.FC<PropsDateBlock> = memo(
-  ({
-    range,
-    handleClickRange,
-    handleMouseRange,
-    targetDay,
-    setTargetDay,
-    setViewTodo,
-    todos
-  }) => {
-    const { typeSwitch, typeStart } = useContext(Context);
-    const [move, setMove] = useState(false);
+const DateBlock: React.FC<PropsDateBlock> = ({
+  range,
+  handleClickRange,
+  handleMouseRange,
+  targetDay,
+  setTargetDay,
+  setViewTodo,
+  todos
+}) => {
+  const { typeSwitch, typeStart } = useContext(Context);
+  const [move, setMove] = useState(false);
 
-    const days =
-      typeStart === StartDays.Mo
-        ? DAYS_OF_WEEK.fromMonady
-        : DAYS_OF_WEEK.fromSunday;
+  const days =
+    typeStart === StartDays.Mo
+      ? DAYS_OF_WEEK.fromMonady
+      : DAYS_OF_WEEK.fromSunday;
 
-    const handleSwiperClick = () => {
-      setMove(true);
-      setTimeout(() => {
-        setMove(false);
-      }, 1000);
-    };
+  const handleSwiperClick = () => {
+    setMove(true);
+    setTimeout(() => {
+      setMove(false);
+    }, 1000);
+  };
 
-    return (
-      <StyledDateBlock $range={!!range}>
-        <Switch
-          handleSwiperClick={handleSwiperClick}
+  return (
+    <StyledDateBlock $range={!!range}>
+      <Switch
+        handleSwiperClick={handleSwiperClick}
+        targetDay={targetDay}
+        setTargetDay={setTargetDay}
+      />
+      {typeSwitch === TypesSwitch.weeks && (
+        <StyledDateBlockStringDays>
+          {days.map((item, index) => {
+            return (
+              <StyledDateBlockStringDaysItem key={index}>
+                {item}
+              </StyledDateBlockStringDaysItem>
+            );
+          })}
+        </StyledDateBlockStringDays>
+      )}
+      {typeSwitch === TypesSwitch.weeks && (
+        <BlockWeeks
+          move={move}
+          range={range}
           targetDay={targetDay}
+          handleClickRange={handleClickRange}
+          handleMouseRange={handleMouseRange}
           setTargetDay={setTargetDay}
+          setViewTodo={setViewTodo}
+          todos={todos}
         />
-        {typeSwitch === TypesSwitch.weeks && (
-          <StyledDateBlockStringDays>
-            {days.map((item, index) => {
-              return (
-                <StyledDateBlockStringDaysItem key={index}>
-                  {item}
-                </StyledDateBlockStringDaysItem>
-              );
-            })}
-          </StyledDateBlockStringDays>
-        )}
-        {typeSwitch === TypesSwitch.weeks && (
-          <BlockWeeks
-            move={move}
-            range={range}
-            targetDay={targetDay}
-            handleClickRange={handleClickRange}
-            handleMouseRange={handleMouseRange}
-            setTargetDay={setTargetDay}
-            setViewTodo={setViewTodo}
-            todos={todos}
-          />
-        )}
-        {typeSwitch === TypesSwitch.months && (
-          <BlockMonths targetDay={targetDay} setTargetDay={setTargetDay} />
-        )}
-        {typeSwitch === TypesSwitch.years && (
-          <BlockYears targetDay={targetDay} setTargetDay={setTargetDay} />
-        )}
-      </StyledDateBlock>
-    );
-  }
-);
-
+      )}
+      {typeSwitch === TypesSwitch.months && (
+        <BlockMonths targetDay={targetDay} setTargetDay={setTargetDay} />
+      )}
+      {typeSwitch === TypesSwitch.years && (
+        <BlockYears targetDay={targetDay} setTargetDay={setTargetDay} />
+      )}
+    </StyledDateBlock>
+  );
+};
 export { DateBlock };
